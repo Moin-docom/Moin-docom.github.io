cc25ba614bdef5350a8069d0304b90630132ea53
Moin-docom <79797706+Moin-docom@users.noreply.github.com>
2024-03-01 08:27:00 +0100

    t

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+        <h1>Anarchismus</h1>
+        <img id="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Circle-A_red.svg/900px-Circle-A_red.svg.png" alt="Company Logo">
+        <p id="styled-quote"><i>Anarchie ist Ordnung</i></p>
+</header>
+
+    <nav>
+        <a href="#">Home</a>
+        <a href="./Präsi.html">Powerpoint Präsi</a>
+        <a href="Weitere-Resourcen.html">Weitere Resourcen zum Anarchismus</a>
+        <a href="#">Musik video Kommunismus</a>
+        <a href="#">Erfahre deine Politische MEinung</a>
+    </nav>
